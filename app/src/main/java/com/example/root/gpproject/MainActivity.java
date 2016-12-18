5f741de8788e445734de381adddd4cb54d16b4cf
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2016-12-19 04:39:45 +0600

    view done

--- app/src/main/java/com/example/root/gpproject/MainActivity.java
+++ app/src/main/java/com/example/root/gpproject/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.root.gpproject;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.example.root.gpproject.Adapters.RecyclerViewAdapter;
+import com.example.root.gpproject.Model.SectionDataModel;
+import com.example.root.gpproject.Model.SingleItemModel;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends NavigationDrawerActivity {
+
+    ArrayList<SectionDataModel> dummydata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,67 +29,52 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        //drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        dummydata=new ArrayList<>();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        createDummyData();
+
+
+        RecyclerView my_recycler_view = (RecyclerView) findViewById(R.id.recycler_view);
+
+        my_recycler_view.setHasFixedSize(true);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, dummydata);
+
+        my_recycler_view.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        my_recycler_view.setAdapter(adapter);
+
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+
+
+
+    public void createDummyData() {
+        for (int i = 1; i <= 5; i++) {
+
+            SectionDataModel dm = new SectionDataModel();
+
+            dm.setHeaderTitle("Section " + i);
+
+            ArrayList<SingleItemModel> singleItem = new ArrayList<SingleItemModel>();
+            for (int j = 0; j <= 5; j++) {
+                singleItem.add(new SingleItemModel("Item " + j, "URL " + j));
+            }
+
+            dm.setAllItemsInSection(singleItem);
+
+            dummydata.add(dm);
+
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
---
+++ app/src/main/java/com/example/root/gpproject/Model/SectionDataModel.java
@@ -0,0 +1,40 @@
+package com.example.root.gpproject.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by root on 12/19/16.
+ */
+
+public class SectionDataModel {
+
+    private String headerTitle;
+    private ArrayList<SingleItemModel> allItemsInSection;
+
+
+    public SectionDataModel() {
+
+    }
+    public SectionDataModel(String headerTitle, ArrayList<SingleItemModel> allItemsInSection) {
+        this.headerTitle = headerTitle;
+        this.allItemsInSection = allItemsInSection;
+    }
+
+
+
+    public String getHeaderTitle() {
+        return headerTitle;
+    }
+
+    public void setHeaderTitle(String headerTitle) {
+        this.headerTitle = headerTitle;
+    }
+
+    public ArrayList<SingleItemModel> getAllItemsInSection() {
+        return allItemsInSection;
+    }
+
+    public void setAllItemsInSection(ArrayList<SingleItemModel> allItemsInSection) {
+        this.allItemsInSection = allItemsInSection;
+    }
+}
